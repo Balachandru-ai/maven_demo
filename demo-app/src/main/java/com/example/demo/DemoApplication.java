804bf6e2aaa9f38fcff4bbbbcfae11f2a4153df0
Balachandru-ai <balachandru@thestackly.com>
2026-02-24 11:17:21 +0530

    Update DemoApplication.java

--- demo-app/src/main/java/com/example/demo/DemoApplication.java
+++ demo-app/src/main/java/com/example/demo/DemoApplication.java
@@ -2,29 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
-import com.example.demo.model.Product;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
-@RequestMapping("/products")
 public class DemoApplication {
-
-    List<Product> productList = new ArrayList<>();
-
-    @GetMapping
-    public List<Product> getAllProducts() {
-        return productList;
-    }
-
-    @PostMapping
-    public String addProduct(@RequestBody Product product) {
-        productList.add(product);
-        return "Product Added Successfully!";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
